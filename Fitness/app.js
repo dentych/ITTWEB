a58bc2eb367b0fdd74d82492aebb76896f4d9785
dentych <dennis@tychsen.me>
2016-11-03 11:21:17 +0100

    Add ze pug stuffs

--- Fitness/app.js
+++ Fitness/app.js
@@ -1,11 +1,14 @@
 "use strict";
 let express = require("express");
 let app = express();
+let morgan = require("morgan");
 
 app.use("/static", express.static("public"));
+app.use(morgan("dev"));
+app.set("view engine", "pug");
 
-app.get("/", function(req, res) {
-    res.send("yup");
+app.get("/", function (req, res) {
+    res.render("index", {title: "Fitness Helper 3000", message: "Welcome to Fitness Helper 3000"});
 });
 
 app.listen(3000, () => {

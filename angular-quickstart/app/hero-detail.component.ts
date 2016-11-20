2450c6043dd2afb07992594fb04f12f08845ea75
dentych <dennis@tychsen.me>
2016-11-20 16:32:07 +0100

    Remove .js files and add service and routing

--- angular-quickstart/app/hero-detail.component.ts
+++ angular-quickstart/app/hero-detail.component.ts
@@ -1,5 +1,10 @@
-import {Component, Input} from "@angular/core";
+import {Component, Input, OnInit} from "@angular/core";
 import {Hero} from "./hero";
+import {ActivatedRoute, Params} from "@angular/router";
+import {Location} from "@angular/common";
+
+import "rxjs/add/operator/switchMap";
+import {HeroService} from "./hero.service";
 
 @Component({
     selector: "my-hero-detail",
@@ -14,7 +19,15 @@ import {Hero} from "./hero";
 </div>
 `
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
     @Input()
     hero: Hero;
+
+    ngOnInit(): void {
+        this.route.params.switchMap((params: Params) => this.heroService.getHero(+params["id"]))
+            .subscribe(hero => this.hero = hero);
+    }
+
+    constructor(private heroService: HeroService, private route: ActivatedRoute, private location: Location) {
+    }
 }
--- angular-quickstart/app/hero.service.ts
+++ angular-quickstart/app/hero.service.ts
@@ -4,7 +4,16 @@ import {Hero} from "./hero";
 
 @Injectable()
 export class HeroService {
-    getHeroes(): Hero[] {
-        return HEROES;
+    getHeroes(): Promise<Hero[]> {
+        return Promise.resolve(HEROES);
+    }
+
+    getHeroesSlowly(): Promise<Hero[]> {
+        return new Promise<Hero[]>(resolve =>
+        setTimeout(resolve, 2000)).then(() => this.getHeroes());
+    }
+
+    getHero(id: number): Promise<Hero> {
+        return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
     }
 }

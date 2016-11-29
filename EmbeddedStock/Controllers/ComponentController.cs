2c38a246b530a1ab1834f0962b8ce3aaec66f97e
dentych <dennis@tychsen.me>
2016-11-29 16:21:42 +0100

    Can now add and show components

--- EmbeddedStock/Controllers/ComponentController.cs
+++ EmbeddedStock/Controllers/ComponentController.cs
@@ -1,9 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using WebApplication.Models.ComponentViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication.Data;
+using WebApplication.Models;
 
 namespace WebApplication.Controllers
 {
     public class ComponentController : Controller
     {
+        private ComponentDbContext context;
 
+        public ComponentController(ComponentDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Create(int componentTypeId)
+        {
+            var model = context.ComponentTypes.Single(ct => ct.ComponentTypeId == componentTypeId);
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Create(long componentTypeId, int componentNumber, string serialNo,
+            ComponentStatus status, string adminComment, string userComment)
+        {
+            var component = new Component();
+            component.ComponentTypeId = componentTypeId;
+            component.ComponentNumber = componentNumber;
+            component.SerialNo = serialNo;
+            component.Status = status;
+            component.AdminComment = adminComment;
+            component.UserComment = userComment;
+
+            context.Components.Add(component);
+            context.SaveChanges();
+
+            return RedirectToAction("Show", new {id = component.ComponentId});
+        }
+
+        public IActionResult Show(int id)
+        {
+            var component = context.Components.Single(c => c.ComponentId == id);
+            var componentType = context.ComponentTypes.Single(ct => ct.ComponentTypeId == component.ComponentTypeId);
+            var model = new ComponentShowViewModel(component, componentType);
+            return View(model);
+        }
     }
 }
